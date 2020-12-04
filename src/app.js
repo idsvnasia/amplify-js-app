ce04e016ffd32b89f54a2ce5fb511c8acd74b071
idsvnasia <thanhnc@idsvn.asia>
2020-12-04 16:30:29 +0000

    commit code in repository

--- src/app.js
+++ src/app.js
@@ -1,26 +1,45 @@
-import Amplify, { API, graphqlOperation } from "@aws-amplify/api";
+ import Amplify, { API, graphqlOperation } from "@aws-amplify/api";
 
-import awsconfig from "./aws-exports";
-import { createTodo } from "./graphql/mutations";
+ import awsconfig from "./aws-exports";
+ import { createTodo } from "./graphql/mutations";
+ import { listTodos } from "./graphql/queries";
++import { onCreateTodo } from "./graphql/subscriptions";
 
-Amplify.configure(awsconfig);
+ Amplify.configure(awsconfig);
 
-async function createNewTodo() {
-  const todo = {
-    name: "Use AppSync",
-    description: `Realtime and Offline (${new Date().toLocaleString()})`,
-  };
+ async function createNewTodo() {
+   const todo = {
+     name: "Use AppSync",
+     description: `Realtime and Offline (${new Date().toLocaleString()})`,
+   };
 
-  return await API.graphql(graphqlOperation(createTodo, { input: todo }));
-}
+   return await API.graphql(graphqlOperation(createTodo, { input: todo }));
+ }
 
-const MutationButton = document.getElementById("MutationEventButton");
-const MutationResult = document.getElementById("MutationResult");
+ async function getData() {
+   API.graphql(graphqlOperation(listTodos)).then((evt) => {
+     evt.data.listTodos.items.map((todo, i) => {
+       QueryResult.innerHTML += `<p>${todo.name} - ${todo.description}</p>`;
+     });
+   });
+ }
 
-MutationButton.addEventListener("click", (evt) => {
-  createNewTodo().then((evt) => {
-    MutationResult.innerHTML += `<p>${evt.data.createTodo.name} - ${evt.data.createTodo.description}</p>`;
-  });
-});
+ const MutationButton = document.getElementById("MutationEventButton");
+ const MutationResult = document.getElementById("MutationResult");
+ const QueryResult = document.getElementById("QueryResult");
++const SubscriptionResult = document.getElementById("SubscriptionResult");
 
-+getData();
+ MutationButton.addEventListener("click", (evt) => {
+   createNewTodo().then((evt) => {
+     MutationResult.innerHTML += `<p>${evt.data.createTodo.name} - ${evt.data.createTodo.description}</p>`;
+   });
+ });
+
++API.graphql(graphqlOperation(onCreateTodo)).subscribe({
++  next: (evt) => {
++    const todo = evt.value.data.onCreateTodo;
++    SubscriptionResult.innerHTML += `<p>${todo.name} - ${todo.description}</p>`;
++  },
++});
++
+ getData();
