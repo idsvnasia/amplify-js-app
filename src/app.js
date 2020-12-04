eb04067bc5857d7b157c015deef5176dc71de1a7
idsvnasia <thanhnc@idsvn.asia>
2020-12-04 18:20:16 +0000

    commit message

--- src/app.js
+++ src/app.js
@@ -2,8 +2,7 @@
 
  import awsconfig from "./aws-exports";
  import { createTodo } from "./graphql/mutations";
- import { listTodos } from "./graphql/queries";
-+import { onCreateTodo } from "./graphql/subscriptions";
++import { listTodos } from "./graphql/queries";
 
  Amplify.configure(awsconfig);
 
@@ -16,18 +15,17 @@
    return await API.graphql(graphqlOperation(createTodo, { input: todo }));
  }
 
- async function getData() {
-   API.graphql(graphqlOperation(listTodos)).then((evt) => {
-     evt.data.listTodos.items.map((todo, i) => {
-       QueryResult.innerHTML += `<p>${todo.name} - ${todo.description}</p>`;
-     });
-   });
- }
-
++async function getData() {
++  API.graphql(graphqlOperation(listTodos)).then((evt) => {
++    evt.data.listTodos.items.map((todo, i) => {
++      QueryResult.innerHTML += `<p>${todo.name} - ${todo.description}</p>`;
++    });
++  });
++}
++
  const MutationButton = document.getElementById("MutationEventButton");
  const MutationResult = document.getElementById("MutationResult");
- const QueryResult = document.getElementById("QueryResult");
-+const SubscriptionResult = document.getElementById("SubscriptionResult");
++const QueryResult = document.getElementById("QueryResult");
 
  MutationButton.addEventListener("click", (evt) => {
    createNewTodo().then((evt) => {
@@ -35,11 +33,4 @@
    });
  });
 
-+API.graphql(graphqlOperation(onCreateTodo)).subscribe({
-+  next: (evt) => {
-+    const todo = evt.value.data.onCreateTodo;
-+    SubscriptionResult.innerHTML += `<p>${todo.name} - ${todo.description}</p>`;
-+  },
-+});
-+
- getData();
++getData();
